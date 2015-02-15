d6ca73502b85204aecf19c327fb9bf1d85dd67e1
mactkg <mactkg@gmail.com>
2015-02-15 20:56:12 +0900

    5-3-6

--- SwiftNews/SwiftNews/ViewController.swift
+++ SwiftNews/SwiftNews/ViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 class ViewController: UITableViewController {
 
   override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    super.viewDidLoad();
+    self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "news")
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCellWithIdentifier("news") as UITableViewCell
+    cell.textLabel!.text = "a news"
+    return cell
   }
 
 
