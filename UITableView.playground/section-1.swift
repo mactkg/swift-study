149dcb6bcc0a58620f2eac4ddfd4e3f2275bb25d
mactkg <mactkg@gmail.com>
2015-02-15 20:46:30 +0900

    5-2-3

--- UITableView.playground/section-1.swift
+++ UITableView.playground/section-1.swift
@@ -3,18 +3,22 @@
 import UIKit
 
 class MyController: UITableViewController {
+  let language = ["Ruby", "Python", "Swift", "Perl", "Objective-C", "C", "Java", "Processing"]
+  let years = [1995, 1991, 2014, 1987, 1983, 1972, 1995, 2001]
+  
   override func viewDidLoad() {
     super.viewDidLoad();
     self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5;
+    return language.count;
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    var cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
-    cell.textLabel!.text = "Swift"
+    var cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "detailCell")
+    cell.textLabel!.text = language[indexPath.row]
+    cell.detailTextLabel!.text = "\(years[indexPath.row])"
     return cell
   }
 }
