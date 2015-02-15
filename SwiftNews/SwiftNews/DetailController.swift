e75cee8c552d693c8982fc124ab989589e459d08
mactkg <mactkg@gmail.com>
2015-02-15 20:50:17 +0900

    start SwiftNews

---
+++ SwiftNews/SwiftNews/DetailController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailController.swift
+//  SwiftNews
+//
+//  Created by Kenta Hara on 2/15/15.
+//  Copyright (c) 2015 Kenta Hara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailController: UIViewController {
+  
+}
---
+++ SwiftNews/SwiftNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftNews
+//
+//  Created by Kenta Hara on 2/15/15.
+//  Copyright (c) 2015 Kenta Hara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
