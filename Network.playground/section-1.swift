1f6050e00e5964a730a6665719ce55a4785bf728
mactkg <mactkg@gmail.com>
2015-02-17 00:51:20 +0900

    6-3-2 fetch data using NSURLSesssion

--- Network.playground/section-1.swift
+++ Network.playground/section-1.swift
@@ -1,12 +1,18 @@
 import UIKit
 import Foundation
+import XCPlayground
 
 var urlString = "http://www.apple.com"
 var url = NSURL(string: urlString)!
 var req = NSURLRequest(URL: url)
 
-var data = NSURLConnection.sendSynchronousRequest(req, returningResponse: nil, error: nil)
-var htmlString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, res, error) -> Void in
+  var string = NSString(data: data, encoding: NSUTF8StringEncoding)!
+})
+
+task.resume()
+
+XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
 
 // this line cause a bug
 // var img = UIImage(data: data!)!
