201c09cde77b446620bcfe1de9bf69fbd335ce6e
mactkg <mactkg@gmail.com>
2015-03-01 03:19:40 +0900

    OSX->iOS / 7-3 get titles using json

--- ParseXML.playground/section-1.swift
+++ ParseXML.playground/section-1.swift
@@ -1,38 +1,24 @@
 // Playground - noun: a place where people can play
 
-import Cocoa
 import Foundation
-
-class MyParserDelegate: NSObject, NSXMLParserDelegate {
-  var isTitle = false
-  
-  func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: [NSObject : AnyObject]!) {
-    if(elementName == "title") {
-      isTitle = true;
-    }
-  }
-  
-  func parser(parser: NSXMLParser!, foundCharacters string: String!) {
-    if(isTitle) {
-      print(string)
-    }
-  }
-  
-  func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
-    if(elementName == "title") {
-      isTitle = false;
-      println()
-    }
-  }
-}
+import UIKit
+import XCPlayground
 
 //var urlString = "http://rss.dailynews.yahoo.co.jp/fc/rss.xml"
-var urlString = "http://www.switch-science.com/catalog/list/1/rss"
-var url = NSURL(string: urlString)
+var urlString = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.switch-science.com/catalog/list/1/rss&num=8"
+var url = NSURL(string: urlString)!
 
-var parser = NSXMLParser(contentsOfURL: url)!
-var delegate = MyParserDelegate()
-parser.delegate = delegate
+var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, res, err) -> Void in
+  var dict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
 
-parser.parse();
+  var responseData = dict["responseData"] as NSDictionary
+  var feed = responseData["feed"] as NSDictionary
+  var entries = feed["entries"] as NSArray
+  
+  for entry in entries {
+    println(entry["title"] as String)
+  }
+})
+task.resume()
 
+XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
